9728b67eb9f5cc4f97b228888ce8935c5cabe1d9
Jirachai Thiemsert <cray.j.thiemsert@gmail.com>
2020-07-31 12:27:06 +0700

    Initial commit

---
+++ lib/bloc/post_bloc.dart
@@ -0,0 +1,76 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:bloc/bloc.dart';
+import 'package:ft_bloc_infinite_list/bloc/bloc.dart';
+import 'package:ft_bloc_infinite_list/models/models.dart';
+import 'package:meta/meta.dart';
+import 'package:http/http.dart' as http;
+import 'package:rxdart/rxdart.dart';
+
+class PostBloc extends Bloc<PostEvent, PostState> {
+  final http.Client httpClient;
+
+  PostBloc({@required this.httpClient}) : super(PostInitial());
+
+
+  @override
+  Stream<Transition<PostEvent, PostState>> transformEvents(
+      Stream<PostEvent> events,
+      TransitionFunction<PostEvent, PostState> transitionFn) {
+    return super.transformEvents(
+      // "debounceTime" came from package:rxdart/rxdart.dart
+      events.debounceTime(const Duration(milliseconds: 500)),
+      transitionFn
+    );
+  }
+
+  @override
+  Stream<PostState> mapEventToState(
+    PostEvent event,
+  ) async* {
+
+    final currentState = state;
+    if(event is PostFetched && !_hasReachedMax(currentState)) {
+      try {
+        if(currentState is PostInitial) {
+          final posts = await _fetchPosts(0, 20);
+          yield PostSuccess(posts: posts, hasReachedMax: false);
+          return;
+        }
+        if(currentState is PostSuccess) {
+          final posts = await _fetchPosts(currentState.posts.length, 20);
+          yield posts.isEmpty
+              ? currentState.copyWith(hasReachedMax: true)
+              : PostSuccess(
+                  posts: currentState.posts + posts,
+                  hasReachedMax: false,
+                );
+        }
+      } catch(_) {
+        yield PostFailure();
+      }
+    } else {
+      print('hasReachedMax = ${_hasReachedMax(currentState)}');
+    }
+  }
+
+  bool _hasReachedMax(PostState state) => state is PostSuccess && state.hasReachedMax;
+
+  Future<List<Post>> _fetchPosts(int startIndex, int limit) async {
+    final response = await httpClient.get('https://jsonplaceholder.typicode.com/posts?_start=$startIndex&_limit=$limit');
+    if(response.statusCode == 200) {
+      // json came from import 'dart:convert'; declaration
+      final data = json.decode(response.body) as List;
+      return data.map((rawPost) {
+        return Post(
+          id: rawPost['id'],
+          title: rawPost['title'],
+          body: rawPost['body'],
+        );
+      }).toList();
+    } else {
+      throw Exception('error fetching posts');
+    }
+  }
+}
